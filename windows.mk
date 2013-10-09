b8d60373a2d6ef2d18833f9bc95f183af6e47017
Tyler Veness <calcmogul@gmail.com>
2013-10-09 01:37:46 -0700

    Renamed module CMakeGroup.wxs to CMakeGroup ; SourceDir variable now passed into heat ; removed sed from windows dependencies

--- windows.mk
+++ windows.mk
@@ -17,23 +17,17 @@ CMAKE_CG=CMakeGroup
 
 all: $(INSTALLERNAME)
 
-$(GENERATE_CG).wxs.in:
-	$(HEAT) dir $(HARVEST_DIR) -sreg -ag -dr INSTALLDIR -cg $(GENERATE_CG) -out $(GENERATE_CG).wxs.in
-
-$(GENERATE_CG).wxs: $(GENERATE_CG).wxs.in
-	sed s!SourceDir\\!SourceDir\\$(HARVEST_DIR)\\!g < $(GENERATE_CG).wxs.in > $(GENERATE_CG).wxs
+$(GENERATE_CG).wxs:
+	$(HEAT) dir $(HARVEST_DIR) -sreg -ag -dr INSTALLDIR -cg $(GENERATE_CG) -var var.HarvestDir -out $(GENERATE_CG).wxs
 
 $(GENERATE_CG).wixobj: $(GENERATE_CG).wxs
-	$(CANDLE) $(GENERATE_CG).wxs
+	$(CANDLE) -dHarvestDir=SourceDir\\$(HARVEST_DIR) $(GENERATE_CG).wxs
 
-$(CMAKE_CG).wxs.in:
-	$(HEAT) dir $(CMAKE_HARVEST) -sreg -ag -dr INSTALLDIR -cg $(CMAKE_CG).wxs -out $(CMAKE_CG).wxs.in
-
-$(CMAKE_CG).wxs: $(CMAKE_CG).wxs.in
-	sed s!SourceDir\\!SourceDir\\$(CMAKE_HARVEST)\\!g < $(CMAKE_CG).wxs.in > $(CMAKE_CG).wxs
+$(CMAKE_CG).wxs:
+	$(HEAT) dir $(CMAKE_HARVEST) -sreg -ag -dr INSTALLDIR -cg $(CMAKE_CG) -var var.CMakeHarvest -out $(CMAKE_CG).wxs
 
 $(CMAKE_CG).wixobj: $(CMAKE_CG).wxs
-	$(CANDLE) $(CMAKE_CG).wxs
+	$(CANDLE) -dCMakeHarvest=SourceDir\\$(CMAKE_HARVEST) $(CMAKE_CG).wxs
 
 Toolchain.wixobj: Toolchain.wxs
 	$(CANDLE) Toolchain.wxs
