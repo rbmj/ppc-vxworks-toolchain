69ab6a273035c6d9e2f4c259bdc7f00d7d87a0d9
Tyler Veness <calcmogul@gmail.com>
2013-10-11 14:03:21 -0700

    Implemented upgrade semantics

--- windows.mk
+++ windows.mk
@@ -2,6 +2,7 @@
 
 #executables:
 
+BUNDLENAME=FRCToolchainInstaller.exe
 INSTALLERNAME=FRCToolchainInstaller.msi
 
 HEAT=heat
@@ -16,22 +17,28 @@ GENERATE_CG=VxWorksToolchainGroup
 CMAKE_HARVEST=win32\\install-prefix\\cmake
 CMAKE_CG=CMakeGroup
 
-all: $(BUILD_DIR)\\$(INSTALLERNAME)
+all: $(BUILD_DIR)\\$(BUNDLENAME)
 
 $(BUILD_DIR)\\$(GENERATE_CG).wxs:
 	$(HEAT) dir $(HARVEST_DIR) -sreg -ag -dr INSTALLDIR -cg $(GENERATE_CG) -var var.HarvestDir -out $(BUILD_DIR)\\$(GENERATE_CG).wxs
 
 $(BUILD_DIR)\\$(GENERATE_CG).wixobj: $(BUILD_DIR)\\$(GENERATE_CG).wxs
-	$(CANDLE) -dHarvestDir=$(CURDIR)\\$(HARVEST_DIR) $(BUILD_DIR)\\$(GENERATE_CG).wxs -out $(BUILD_DIR)\\$(GENERATE_CG).wixobj
+	$(CANDLE) -dHarvestDir=SourceDir\\..\\$(HARVEST_DIR) $(BUILD_DIR)\\$(GENERATE_CG).wxs -out $(BUILD_DIR)\\$(GENERATE_CG).wixobj
 
 $(BUILD_DIR)\\$(CMAKE_CG).wxs:
 	$(HEAT) dir $(CMAKE_HARVEST) -sreg -ag -dr INSTALLDIR -cg $(CMAKE_CG) -var var.CMakeHarvest -out $(BUILD_DIR)\\$(CMAKE_CG).wxs
 
 $(BUILD_DIR)\\$(CMAKE_CG).wixobj: $(BUILD_DIR)\\$(CMAKE_CG).wxs
-	$(CANDLE) -dCMakeHarvest=$(CURDIR)\\$(CMAKE_HARVEST) $(BUILD_DIR)\\$(CMAKE_CG).wxs -out $(BUILD_DIR)\\$(CMAKE_CG).wixobj
+	$(CANDLE) -dCMakeHarvest=SourceDir\\..\\$(CMAKE_HARVEST) $(BUILD_DIR)\\$(CMAKE_CG).wxs -out $(BUILD_DIR)\\$(CMAKE_CG).wixobj
 
-$(BUILD_DIR)\\Toolchain.wixobj: Toolchain.wxs
+$(BUILD_DIR)\\Toolchain.wixobj: Toolchain.wxs Properties.wxi
 	$(CANDLE) Toolchain.wxs -out $(BUILD_DIR)\\Toolchain.wixobj
 
 $(BUILD_DIR)\\$(INSTALLERNAME): $(BUILD_DIR)\\Toolchain.wixobj $(BUILD_DIR)\\$(GENERATE_CG).wixobj $(BUILD_DIR)\\$(CMAKE_CG).wixobj
 	$(LIGHT) -ext WixUIExtension $(BUILD_DIR)\\Toolchain.wixobj $(BUILD_DIR)\\$(GENERATE_CG).wixobj $(BUILD_DIR)\\$(CMAKE_CG).wixobj -out $(BUILD_DIR)\\$(INSTALLERNAME)
+
+$(BUILD_DIR)\\Bundle.wixobj: Bundle.wxs Properties.wxi
+	$(CANDLE) Bundle.wxs -ext WixBalExtension -out $(BUILD_DIR)\\Bundle.wixobj
+
+$(BUILD_DIR)\\$(BUNDLENAME): $(BUILD_DIR)\\$(INSTALLERNAME) $(BUILD_DIR)\\Bundle.wixobj
+	$(LIGHT) -ext WixBalExtension $(BUILD_DIR)\\Bundle.wixobj -out $(BUILD_DIR)\\$(BUNDLENAME)
